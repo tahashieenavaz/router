312e9a97400974f6152c4dbe91f70e27e46e16ca
Taha Shieenavaz <tahashieenavaz@gmail.com>
2022-05-16 13:26:04 +0430

    Router functionality completed

--- src/Router.php
+++ src/Router.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace TahaShieenavaz\MinimalRouter;
+
 class Router {
     private static array $routes = [
         "get" => [],
@@ -15,17 +17,31 @@ class Router {
         return $path;
     }
 
-    public static function get(string $path, Closure|string $callback): void {
+    public static function get(string $path, \Closure|string $callback): void {
         $path = self::preparePurePath($path);
         self::$routes['get'][$path] = $callback;
     }
 
-    public static function post(string $path, Closure|string $callback): void {
+    public static function post(string $path, \Closure|string $callback): void {
         $path = self::preparePurePath($path);
         self::$routes['post'][$path] = $callback;
     }
 
     public static function dispatch(): void {
+        $currentRoute = trim($_SERVER['REQUEST_URI'], '/');
+        $currentRequestMethod = strtolower($_SERVER['REQUEST_METHOD']);
 
+        $routePatterns = array_keys(self::$routes[$currentRequestMethod]);
+
+        foreach($routePatterns as $index => $routePattern) {
+            $routePattern = trim($routePattern, '/');
+            $routePattern = preg_replace('/{(.*?)}/', '(.*)', $routePattern);
+
+            if(preg_match("%^{$routePattern}$%", $currentRoute, $matches) == 0)
+                continue;
+
+            array_shift($matches);
+            echo call_user_func(self::$routes[$currentRequestMethod][$routePatterns[$index]], ...$matches);
+        }
     }
 }
