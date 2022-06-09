d9edfb1ac3b31cadb599cf5fc7fdc805d3682902
Taha Shieenavaz <tahashieenavaz@gmail.com>
2022-06-09 18:00:34 +0430

    array_key_exists was not being used correctly

--- src/helpers.php
+++ src/helpers.php
@@ -1,10 +1,10 @@
 <?php
 
 function arg($name) {
-    if( array_key_exists($_POST, $name) )
+    if( array_key_exists($name, $_POST) )
         return $_POST[$name];
 
-    if( array_key_exists($_GET, $name) )
+    if( array_key_exists($name, $_GET) )
         return $_GET[$name];
 
     return false;
